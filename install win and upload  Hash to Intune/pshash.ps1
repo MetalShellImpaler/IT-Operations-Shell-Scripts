ba61106628d5a3270d947acad3105db896105e2e
A.Russo <85824539+MetalShellImpaler@users.noreply.github.com>
2024-08-18 08:57:00 +0300

    Add files via upload

---
+++ install win and upload  Hash to Intune/pshash.ps1
@@ -0,0 +1,12 @@
+﻿#Enable script
+#Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+#Get serial number
+$SN = Get-WmiObject win32_bios | select Serialnumber
+#Converts serial number to string variable
+$SN = [string]$SN
+$Position = $SN.IndexOf("=")
+$SN = $SN.Substring(($Position+1))
+$SN_String = $SN.Remove(8,1)
+$SN_String = $SN_String+'.csv'
+#Hash
+powershell -noprofile -executionpolicy unrestricted -command d:\hash\autopilot.ps1 -computername $env:COMPUTERNAME -outputfile d:\hash\hash.csv -append #use USB path
