f0d714692ea2bd8239a28965dd19b7bdb4eae059
A.Russo <85824539+MetalShellImpaler@users.noreply.github.com>
2024-08-18 09:02:42 +0300

    Update pshash.ps1

--- install win and upload  Hash to Intune/pshash.ps1
+++ install win and upload  Hash to Intune/pshash.ps1
@@ -8,5 +8,7 @@ $Position = $SN.IndexOf("=")
 $SN = $SN.Substring(($Position+1))
 $SN_String = $SN.Remove(8,1)
 $SN_String = $SN_String+'.csv'
+
+# https://github.com/MikePohatu/Get-WindowsAutoPilotInfo/blob/main/Get-WindowsAutoPilotInfo.ps1
 #Hash
 powershell -noprofile -executionpolicy unrestricted -command d:\hash\autopilot.ps1 -computername $env:COMPUTERNAME -outputfile d:\hash\hash.csv -append #use USB path
