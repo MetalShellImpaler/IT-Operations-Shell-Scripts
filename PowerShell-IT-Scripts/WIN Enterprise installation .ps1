5c5e6ea4de3bdac84293e8bd58b7d79ea1176ef0
A.Russo <85824539+MetalShellImpaler@users.noreply.github.com>
2024-08-21 11:19:31 +0300

    Add files via upload

---
+++ PowerShell-IT-Scripts/WIN Enterprise installation .ps1
@@ -0,0 +1,6 @@
+# download MediaCreationTool
+# CD to MediaCreationTool location.
+# Run this commend in Powershell - Admin
+.\MediaCreationTool_Win11_23H2.exe /Eula Accept /Retail /MediaArch x64 /MediaLangCode en-US /MediaEdition Enterprise
+#enter key
+# for example: NPPR9-FWDCX-D2C8J-H872K-2YT43
