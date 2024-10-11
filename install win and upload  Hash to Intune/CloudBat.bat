990720fdd2119fe899033e57830936b8e87a9bd2
A.Russo <85824539+MetalShellImpaler@users.noreply.github.com>
2024-10-11 14:31:47 +0300

    Update CloudBat.bat

--- install win and upload  Hash to Intune/CloudBat.bat
+++ install win and upload  Hash to Intune/CloudBat.bat
@@ -9,6 +9,11 @@ powercfg -change -monitor-timeout-ac 0
 powercfg -change -monitor-timeout-dc 0
 powercfg /change standby-timeout-ac 0
 powercfg /change standby-timeout-dc 0
+::optional:
+::powercfg /change hibernate-timeout-ac 0
+::powercfg /change hibernate-timeout-dc 0
+::powercfg /SETDCVALUEINDEX SCHEME_CURRENT 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e seconds
+::powercfg /SETACVALUEINDEX SCHEME_CURRENT 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e seconds
 
 : : Show SERIALNUMBER
 WMIC BIOS GET SERIALNUMBER
