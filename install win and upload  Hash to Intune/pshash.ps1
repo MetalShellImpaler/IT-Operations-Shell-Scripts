9ce8226df1d574047b4d00c21bd5c940dc6ec4c5
Anael Russo <85824539+MetalShellImpaler@users.noreply.github.com>
2025-01-13 18:50:44 +0200

    Update pshash.ps1

--- install win and upload  Hash to Intune/pshash.ps1
+++ install win and upload  Hash to Intune/pshash.ps1
@@ -5,6 +5,9 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 #Download the Get-WindowsAutoPilotInfo.ps1 file:
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Save-Script -Name Get-WindowsAutoPilotInfo -Path C:\Users\USERNAME\Downloads
+# Or
+Save-Script -Name Get-WindowsAutoPilotInfo -Path "C:\Users\$env:USERNAME\Downloads"
+
 
 # official source: https://www.powershellgallery.com/packages/Get-WindowsAutoPilotInfo/3.8/Content/Get-WindowsAutopilotInfo.ps1
 # On Github:  https://github.com/MikePohatu/Get-WindowsAutoPilotInfo/blob/main/Get-WindowsAutoPilotInfo.ps1
