a167d898f4c27f2a5d9843efb787b6efd938db0a
Anael Russo <85824539+MetalShellImpaler@users.noreply.github.com>
2025-01-13 17:57:47 +0200

    Update pshash.ps1

--- install win and upload  Hash to Intune/pshash.ps1
+++ install win and upload  Hash to Intune/pshash.ps1
@@ -1,14 +1,8 @@
 # Generate hardware hash csv file.
 # Enable script
 #Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
-#Get serial number
-$SN = Get-WmiObject win32_bios | select Serialnumber
+
 #Converts serial number to string variable
-$SN = [string]$SN
-$Position = $SN.IndexOf("=")
-$SN = $SN.Substring(($Position+1))
-$SN_String = $SN.Remove(8,1)
-$SN_String = $SN_String+'.csv'
 
 # official source: https://www.powershellgallery.com/packages/Get-WindowsAutoPilotInfo/3.8/Content/Get-WindowsAutopilotInfo.ps1
 # On Github:  https://github.com/MikePohatu/Get-WindowsAutoPilotInfo/blob/main/Get-WindowsAutoPilotInfo.ps1
