92f83dbdd0f4398408cae0f518511d3267bd29bd
Anael Russo <85824539+MetalShellImpaler@users.noreply.github.com>
2025-01-15 10:38:20 +0200

    Update updatewin.ps1

--- PowerShell-IT-Scripts/updatewin.ps1
+++ PowerShell-IT-Scripts/updatewin.ps1
@@ -3,7 +3,6 @@
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 
 #Current user only Execution Policy: RemoteSigned
-
 #Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
