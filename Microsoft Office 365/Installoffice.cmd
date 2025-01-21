c91437894050a5d2b405789a7140d78e77bc355d
Anael Russo <85824539+MetalShellImpaler@users.noreply.github.com>
2025-01-21 10:37:52 +0200

    Add files via upload

---
+++ Microsoft Office 365/Installoffice.cmd
@@ -0,0 +1,17 @@
+:: Download office:
+:: Office Deployment Tool
+:: https://www.microsoft.com/en-us/download/details.aspx?id=49117
+
+:: Open CMD as admin 
+:: CD to the files location.
+:: If files are on another drive - go to that drive (run:  D: or F: )
+
+:: Run this command to install office 365:
+setup /configure configuration-Office365-x64.xml
+
+:: optional:
+
+:: Activate office with Massgrave:
+:: https://massgrave.dev/
+:: https://github.com/massgravel/Microsoft-Activation-Scripts
+
