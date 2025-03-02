6f4bb7b6a7dec81d9f77b3175792d4a67a4455d3
Anael Russo <85824539+MetalShellImpaler@users.noreply.github.com>
2025-03-02 11:41:32 +0200

    Update CloudBat.bat

--- Generate hardware hash and upload it to Intune/CloudBat.bat
+++ Generate hardware hash and upload it to Intune/CloudBat.bat
@@ -35,8 +35,15 @@ WMIC BIOS GET SERIALNUMBER
 :: official source: https://www.powershellgallery.com/packages/Get-WindowsAutoPilotInfo/3.8/Content/Get-WindowsAutopilotInfo.ps1
 :: On Github:  https://github.com/MikePohatu/Get-WindowsAutoPilotInfo/blob/main/Get-WindowsAutoPilotInfo.ps1
 @echo off
-:: "d:\hash\hash.csv" - The CSV file will be stored at this location. 
-powershell -noprofile -executionpolicy unrestricted -command d:\hash\Get-WindowsAutopilotInfo.ps1 -OutputFile "D:\hash\Hash.csv" -Append
+:: "d:\hash\.csv" - The CSV file will be stored at this location. 
+
+powershell -noprofile -executionpolicy unrestricted -command ^
+"$SerialNumber = (Get-WmiObject -Class Win32_BIOS).SerialNumber; ^
+$OutputFile = \"D:\hash\$SerialNumber.csv\"; ^
+D:\hash\Get-WindowsAutopilotInfo.ps1 -OutputFile $OutputFile"
+
+:: Other option:
+:: powershell -noprofile -executionpolicy unrestricted -command d:\hash\Get-WindowsAutopilotInfo.ps1 -OutputFile "D:\hash\Hash.csv" -Append
 
 :: Confirm-SecureBoot
 powershell -Command "Confirm-SecureBootUEFI"
