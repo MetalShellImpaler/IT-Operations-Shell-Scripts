844b1fd2718e7a1e08bf6d030118518fb43e2f4b
Anael Russo <85824539+MetalShellImpaler@users.noreply.github.com>
2025-04-10 13:38:52 +0300

    Add files via upload

---
+++ Intune Endpoint Debugging.ps1
@@ -0,0 +1,18 @@
+# Check Intune policy assignment
+dsregcmd /status
+
+# Check applied policies
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\Current\Device\*"
+
+# Logs related to Intune MDM activities
+Get-WinEvent -LogName "Microsoft-Windows-DeviceManagement-Enterprise-Diagnostics-Provider/Operational" -MaxEvents 50 |
+Select-Object TimeCreated, Id, LevelDisplayName, Message
+
+# Generate full Intune report
+mdmdiagnosticstool -out C:\IntuneReport.zip
+
+# Check IME logs manually
+C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\IntuneManagementExtension.log
+
+# Intune Sync
+Start-Process -FilePath "C:\Program Files (x86)\Microsoft Intune Management Extension\Microsoft.Management.Services.IntuneWindowsAgent.exe" -ArgumentList "intunemanagementextension://synccompliance"
