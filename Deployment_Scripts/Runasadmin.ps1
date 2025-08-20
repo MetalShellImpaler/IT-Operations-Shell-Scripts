c8e54a02960bb0e2f99b79e2734a0d05d2d4185b
Anael Russo <85824539+MetalShellImpaler@users.noreply.github.com>
2025-08-20 10:54:51 +0300

    Update Runasadmin.ps1

--- Deployment_Scripts/Runasadmin.ps1
+++ Deployment_Scripts/Runasadmin.ps1
@@ -1,9 +1,8 @@
 # Run as admin
-$scriptPath = (Get-Item -Path $MyInvocation.MyCommand.Definition).FullName
-
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
-).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+$scriptPath = $MyInvocation.MyCommand.Definition
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
-
+    exit
 }
+
 
